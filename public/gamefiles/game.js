2f51dab8d1381891c10fb999b8ce52ad8a679ca2
Max Puchstein <maximilian.puchstein@sandstorm.de>
2020-07-28 12:50:01 +0200

    more comments

--- public/gamefiles/game.js
+++ public/gamefiles/game.js
@@ -21,19 +21,24 @@ let serverData;
 socketConnection.onmessage = mess => {
     //turning the recieved message into useable JSON
     serverData = JSON.parse(mess.data);
-    //console.log(serverData);
+    //checking for changes in the user list
     if (users.length <= serverData.users.length) {
+        //updates users list when new player joins
         users = serverData.users;
     } else {
+        //checks if the list is still the same
         for (let i = 0; i < users.length; i++) {
+            //user currently processed
             const gameUser = users[i];
             let stillPlaying = false;
+            //checking if te user is still playing
             for (let k = 0; k < serverData.users.length; k++) {
                 const serverUser = serverData.users[k];
                 if (gameUser == serverUser) {
                     stillPlaying = true;
                 }
             }
+            //if the user is no longer playing he will be removed
             if(!stillPlaying) {
                 players[users[i]].destroy();
                 delete players[users[i]];
@@ -41,6 +46,7 @@ socketConnection.onmessage = mess => {
             }
         }
     }
+    //updating playerdata with output from the server
     if (serverData[username]) {
         player.x = serverData[username][0];
         player.y = serverData[username][1];
@@ -99,37 +105,45 @@ function create() {
     //adding colliders
     this.physics.add.collider(ball, player);
     //inputs
+    //locking the mouse while playing
     this.input.on('pointerdown', function (pointer) {
         this.input.mouse.requestPointerLock();
     }, this);
+    //controlling the player while in locked state
     this.input.on('pointermove', function (pointer) {
         if(this.input.mouse.locked) {
+            //setting the target sligthly before the player object so prevent overshooting of the mouse
             let x = player.x + 2 * pointer.movementX;
             let y = player.y + 2 * pointer.movementY;
-            player.body.moves = true;
+            //calculating the overall pointer velocity so we can work with it
             let pointerVel = Math.sqrt(Math.pow(pointer.movementX, 2) + Math.pow(pointer.movementY, 2));
+            //setting a maximum for the velocity
             if(pointerVel > 400) {pointerVel = 400}
+            //move the player object. Needs to be done like that as collisions are not applied otherwise
             this.physics.moveTo(player, x, y, 100 + 10 * pointerVel);
-            //player.x = pointer.x;
-            //player.y = pointer.y;
         }
     }, this);
+    //undo lock
     this.input.keyboard.on('keydown_Q', function (event) {
-        if (this.input.mouse.locked)
-        {
+        if (this.input.mouse.locked) {
             this.input.mouse.releasePointerLock();
         }
     }, this);
 } 
 
 function update() {
+    //setting player velocity to 0 if there is no input
     player.body.velocity = new Phaser.Math.Vector2;
     //creating sprites for other players
     for (let i=0; i < users.length; i++) {
+        //user currently processed
         const user = users[i];
+        //add gameobject if it doesnt exist already
         if (!(users[i] == username)&&!(players[user])) {
-            console.log('new sprite update');
-            const sprite = this.add.sprite(500, 500, 'player').setDisplaySize(32, 32);
+            const sprite = this.add.sprite(500, 500, 'player').setDisplaySize(32, 32).setCollideWorldBounds(true);
+            sprite.body.isCircle = true;
+            sprite.body.immovable = true;
+            //add oameobject to list of current users
             players[user] = sprite;
         }
     }
